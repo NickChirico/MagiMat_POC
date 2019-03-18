c98ed2259e91fc8da360cc9edcbf4361189d762c
Kevin Ray <kr1995@nyu.edu>
2019-03-18 00:40:05 -0400

    TileMap && Beginning of FireToad

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    
     protected Rigidbody2D _rigidbody2D;
     protected SpriteRenderer _spriteRenderer;
     
@@ -14,4 +15,59 @@ public class Enemy : MonoBehaviour
     }
 
     
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
+
+public class FireToad : Enemy
+{
+    [HideInInspector] public bool _isGrounded;
+    [HideInInspector] public bool _isJumping;
+    public float jumpPower;
+    public GameObject Fireball;
+    
+    void Update()
+    {
+        //
+        if (_isGrounded && _isJumping)
+        {
+            StartCoroutine(JumpAttack());
+        }
+    }
+    void Jump(float power)
+    {
+        //add upward force for jump
+        //set y velocity to 0 for consistent jump height even if there was previously a downward velocity
+        
+        Vector2 velocity = _rigidbody2D.velocity;
+        velocity.y = 0;
+        _rigidbody2D.velocity = velocity;
+        _rigidbody2D.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+    }
+
+    IEnumerator JumpAttack()
+    {
+        _isJumping = true;
+        float JumpWait = 3;
+        float AttackWait = Random.Range(.2f, .3f);
+        yield return new WaitForSeconds(JumpWait);
+        Jump(jumpPower);
+        yield return new WaitForSeconds(AttackWait);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            GameObject FireballClone = Instantiate(Fireball, transform.position, Quaternion.identity);
+        }
+    }
 }
